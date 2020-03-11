86030347ffa9fe7437e4b3e83295eacb89af8515
Yongxin_Lei Lei <Yongxin.Lei@iam.local>
2020-03-11 16:25:44 -0500

    add code

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+#define MAX_CMD_LEN  128
+#define HISTORY_COUNT 20
+
 int main(){
 
     int floors [16] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
@@ -10,8 +13,18 @@ int main(){
     int playerFloor;
     int checkValidFloor = 0;
 
+    int count=1;
+
+    char cmd[MAX_CMD_LEN];
+    char *hist[HISTORY_COUNT];
+    int i, current = 0;
+
+
     printf("Choose a floor: ");
     scanf("\n%d", &playerFloor);
+
+    for (i = 0; i < HISTORY_COUNT; i++)
+                hist[i] = NULL;
 
     for(int i = 0; i < size; i++)
     {
@@ -36,11 +49,30 @@ int main(){
             }
         }
     }
+
     else
     {
         printf("Invalid Floor Inserted !");
     }
     
-
     return 0;
+    
+}
+
+int history(char *hist[], int current)
+{
+        int i = current;
+        int hist_num = 1;
+
+        do {
+                if (hist[i]) {
+                        printf("%4d  %s\n", hist_num, hist[i]);
+                        hist_num++;
+                }
+
+                i = (i + 1) % HISTORY_COUNT;
+
+        } while (i != current);
+
+        return 0;
 }
