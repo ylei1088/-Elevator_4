cb783167d2d80a45dbda585643bc62bc8cc96d28
Yongxin_Lei Lei <Yongxin.Lei@iam.local>
2020-03-12 16:21:02 -0500

    made changes

--- main.c
+++ main.c
@@ -83,21 +83,3 @@ int main(){
 
     return 0;    
 }
-
-// int history(char *hist[], int current)
-// {
-//         int i = current;
-//         int hist_num = 1;
-
-//         do {
-//                 if (hist[i]) {
-//                         printf("%4d  %s\n", hist_num, hist[i]);
-//                         hist_num++;
-//                 }
-
-//                 i = (i + 1) % HISTORY_COUNT;
-
-//         } while (i != current);
-
-//         return 0;
-// }
