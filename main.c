19eeb64ccae3cba7dd2161f5a958decfd41a1079
Yongxin_Lei Lei <Yongxin.Lei@iam.local>
2020-03-12 16:19:51 -0500

    made changes

--- main.c
+++ main.c
@@ -9,70 +9,95 @@ int main(){
     int floors [16] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
     int size = sizeof(floors)/sizeof(floors[0]);
 
+    int storage[5];
+    int storageSize = sizeof(storage)/sizeof(storage[0]);
+
     int currentFloor;
     int playerFloor;
     int checkValidFloor = 0;
 
-    int count=1;
+    int playerCount = 0;
+
+    int count = 1;
 
     char cmd[MAX_CMD_LEN];
     char *hist[HISTORY_COUNT];
     int i, current = 0;
 
-
-    printf("Choose a floor: ");
-    scanf("\n%d", &playerFloor);
-
-    for (i = 0; i < HISTORY_COUNT; i++)
-                hist[i] = NULL;
-
-    for(int i = 0; i < size; i++)
-    {
-        if(playerFloor == floors[i])
+    //for (i = 0; i < HISTORY_COUNT; i++)
+    //hist[i] = NULL;
+    
+    while(playerCount < 5)
+    {   
+        do
         {
-            checkValidFloor = 1;
-        }
-    }
+            printf("Choose a floor: ");
+            scanf("\n%d", &playerFloor);
+            
+        }while(playerFloor < 0 || playerFloor > 16);
 
-    if(checkValidFloor == 1)
-    {
+        for(int i = playerCount; i < storageSize; i++)
+        {  
+           storage[i] = playerFloor;
+           break;
+        }
+
         for(int i = 0; i < size; i++)
         {
-            if(playerFloor > i)
+            if(playerFloor == floors[i])
             {
-                printf("%d\n",i);   
-            }
-            else if(playerFloor == i)
-            {
-                printf("%d\n",i);
-                printf("DING!\n");
+                checkValidFloor = 1;
             }
         }
-    }
 
-    else
-    {
-        printf("Invalid Floor Inserted !");
-    }
-    
-    return 0;
-    
-}
-
-int history(char *hist[], int current)
-{
-        int i = current;
-        int hist_num = 1;
-
-        do {
-                if (hist[i]) {
-                        printf("%4d  %s\n", hist_num, hist[i]);
-                        hist_num++;
+        if(checkValidFloor == 1)
+        {
+            for(int i = 0; i < size; i++)
+            {
+                if(playerFloor > i)
+                {
+                    printf("%d\n",i);   
                 }
+                else if(playerFloor == i)
+                {
+                    printf("%d\n",i);
+                    printf("DING!\n");
+                    playerCount++;
+                }
+            }
+        }
+        else
+        {
+            printf("Invalid Floor Inserted !");
+        }        
+    }
 
-                i = (i + 1) % HISTORY_COUNT;
+    for(int i = 0; i < storageSize; i++)
+    {
+        if(i == 0)
+        {
+            printf("\nHistory\n");
+        }
+        printf("%d", storage[i]);
+    }
 
-        } while (i != current);
-
-        return 0;
+    return 0;    
 }
+
+// int history(char *hist[], int current)
+// {
+//         int i = current;
+//         int hist_num = 1;
+
+//         do {
+//                 if (hist[i]) {
+//                         printf("%4d  %s\n", hist_num, hist[i]);
+//                         hist_num++;
+//                 }
+
+//                 i = (i + 1) % HISTORY_COUNT;
+
+//         } while (i != current);
+
+//         return 0;
+// }
